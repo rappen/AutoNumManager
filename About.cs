f6d248c78eb716f43ed126b561835d17b43ae94f
Jonas Rapp <jonas.rapp@innofactor.com>
2017-10-25 15:59:48 +0200

    Adding assembly version info to About box

--- About.cs
+++ About.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace Rappen.XTB.AutoNumManager
@@ -17,6 +20,7 @@ namespace Rappen.XTB.AutoNumManager
         {
             InitializeComponent();
             this.autoNumMgr = autoNumMgr;
+            PopulateAssemblies();
         }
 
         #endregion Public Constructors
@@ -53,6 +57,43 @@ namespace Rappen.XTB.AutoNumManager
         {
             autoNumMgr.LogUse("About-OpenTwitter");
             System.Diagnostics.Process.Start("http://twitter.com/rappen");
+        }
+
+        private void PopulateAssemblies()
+        {
+            var assemblies = GetReferencedAssemblies();
+            var items = assemblies.Select(a => GetListItem(a)).ToArray();
+            listAssemblies.Items.Clear();
+            listAssemblies.Items.AddRange(items);
+        }
+
+        private ListViewItem GetListItem(AssemblyName a)
+        {
+            var item = new ListViewItem(a.Name);
+            item.SubItems.Add(a.Version.ToString());
+            return item;
+        }
+
+        private List<AssemblyName> GetReferencedAssemblies()
+        {
+            var names = Assembly.GetExecutingAssembly().GetReferencedAssemblies()
+                    .Where(a => !a.Name.Equals("mscorlib") && !a.Name.StartsWith("System") && !a.Name.Contains("CSharp")).ToList();
+            names.Add(Assembly.GetEntryAssembly().GetName());
+            names.Add(Assembly.GetExecutingAssembly().GetName());
+            names = names.OrderBy(a => assemblyPrioritizer(a.Name)).ToList();
+            return names;
+        }
+
+        private static string assemblyPrioritizer(string assemblyName)
+        {
+            return
+                assemblyName.Equals("XrmToolBox") ? "AAAAAAAAAAAA" :
+                assemblyName.Contains("XrmToolBox") ? "AAAAAAAAAAAB" :
+                assemblyName.Equals(Assembly.GetExecutingAssembly().GetName().Name) ? "AAAAAAAAAAAC" :
+                assemblyName.Contains("Jonas") ? "AAAAAAAAAAAD" :
+                assemblyName.Contains("Innofactor") ? "AAAAAAAAAAAE" :
+                assemblyName.Contains("Cinteros") ? "AAAAAAAAAAAF" :
+                assemblyName;
         }
 
         #endregion Private Methods
